7fcf3485f36b7789f1c41de762e46932f044e627
hackspb <hackspb@gmail.com>
2018-12-20 17:43:33 +0300

    запись в новую

--- src/App.js
+++ src/App.js
@@ -2,66 +2,15 @@ import React from 'react';
 import connect from '@vkontakte/vkui-connect';
 import { Alert, View, Panel, PanelHeader, Group, InfoRow,FormLayout, FormLayoutGroup, Input,Select,Radio,Textarea,Checkbox, Link, Button,Div, Slider} from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
+ import super_var from './test.txt';
 
-/*
-function serialize(form)
-{
-	if (!form || form.nodeName !== "FORM") {
-		return;
-	}
-	var i, j,
-		obj = {'test':1};
-	for (i = form.elements.length - 1; i >= 0; i = i - 1) {
-		if (form.elements[i].name === "") {
-			continue;
-		}
-		switch (form.elements[i].nodeName) {
-		case 'INPUT':
-			switch (form.elements[i].type) {
-			case 'text':
-			case 'hidden':
-			case 'password':
-			case 'button':
-			case 'reset':
-			case 'submit':
-				obj[form.elements[i].name] = encodeURIComponent(form.elements[i].value);
-				break;
-			case 'checkbox':
-			case 'radio':
-				if (form.elements[i].checked) {
-					obj[form.elements[i].name] = encodeURIComponent(form.elements[i].value);
-				}
-				break;
-			case 'file':
-				break;
-			}
-			break;
-		case 'TEXTAREA':
-			obj[form.elements[i].name] = encodeURIComponent(form.elements[i].value);
-			break;
-		case 'SELECT':
-			switch (form.elements[i].type) {
-			case 'select-one':
-				obj[form.elements[i].name] = encodeURIComponent(form.elements[i].value);
-				break;
-			case 'select-multiple':
-				for (j = form.elements[i].options.length - 1; j >= 0; j = j - 1) {
-					if (form.elements[i].options[j].selected) {
-						obj[form.elements[i].name] = encodeURIComponent(form.elements[i].options[j].value);
-					}
-				}
-				break;
-			}
-			break;
-
-		}
-	}
-	return obj;
-}*/
+require('./test.txt');
 
 var user = {first_name: "",last_name:"", sex:0, id:0};
 var valueold =""
 
+
+console.dir(super_var);
 
 var val = (elem) => {
 		console.log(elem)
@@ -74,7 +23,7 @@ class App extends React.Component {
 
 		this.state = {
 			popout: null,
-			activePanel: 'newuser',
+			activePanel: 'home',
 			fetchedUser: null,
 			formReg: {
 				sex:0,
@@ -135,8 +84,8 @@ class App extends React.Component {
 	ajaxSend=()=>{
 		var data = new FormData();
 		data.append( "json", JSON.stringify( this.state.formReg ) );
-
-		fetch("https://ftvkapp.beget.tech/saveUser.php", {
+/* https://ftvkapp.beget.tech/saveUser.php */
+		fetch("https://localhost/saveUser.php", {
     method: 'post',
     body: data
   })
@@ -176,12 +125,50 @@ let $=this.state.formReg;
           </InfoRow>
 				</Div>
 
+				<FormLayout id="form3">
+					<Input top="Калории по планке" name="caloriePlank"  value={$.caloriePlank} onChange={this.ChangeSt}/>
+					<Input top="Средний вес за неделю" name="averageWeight" value={$.averageWeight} onChange={this.ChangeSt}/>
+						<Input top="Минимальный вес за неделю" name="minWeight" value={$.minWeight} onChange={this.ChangeSt}/>
+							<Select top="Тренировки (оцените по шкале от 0 до 5, где 0-не тренировались, 5 - активно тренировались)"
+								placeholder="Ваша оценка"  name="rateTrainings"  value={$.rateTrainings}  onChange={this.ChangeSt}>
+							 <option value="1">1</option>
+							 <option value="2">1</option>
+							 <option value="3">3</option>
+							 <option value="4">4</option>
+							 <option value="5">5</option>
+						 </Select>
+
+						 <Select top="Питание (оцените по шкале 0-5 насколько точно вы придерживались макросов и рекомендаций)"
+							  placeholder="Ваша оценка"  name="rateNutrition"  value={$.rateNutrition}  onChange={this.ChangeSt}>
+							<option value="1">1</option>
+							<option value="2">1</option>
+							<option value="3">3</option>
+							<option value="4">4</option>
+							<option value="5">5</option>
+						</Select>
+				</FormLayout>
+
+
 	      <Div>
            <Button level="commerce" onClick={() =>this.setState({activePanel: 'newuser'})} >Регистрация</Button>
 				</Div>
 
 
    </Panel>
+	 <Panel id="everyWeek" theme="white">
+     <PanelHeader>Фитнес</PanelHeader>
+
+         <Div><InfoRow title="">
+            Еженедельный отчет
+           </InfoRow>
+ 				</Div>
+      <Div>
+            <Button level="commerce" onClick={() =>this.setState({activePanel: 'newuser'})} >Регистрация</Button>
+ 				</Div>
+
+
+    </Panel>
+
    <Panel id="newuser" theme="white">
     <PanelHeader>Регистрация</PanelHeader>
     	 <Group>
@@ -196,9 +183,9 @@ let $=this.state.formReg;
 			<Input top="Имя" name="first_name"  value={$.first_name} onChange={this.ChangeSt}/>
       <Input top="Фамилия" name="last_name" value={$.last_name} onChange={this.ChangeSt}/>
 
-       <Select top="Пол" placeholder="Выберите пол"  name="sex" value={($.sex==2)?'m':'f'} onChange={this.ChangeSt}>
-        <option value="m">Мужской</option>
-        <option value="f">Женский</option>
+       <Select top="Пол" placeholder="Выберите пол"  name="sex" value={$.sex} onChange={this.ChangeSt}>
+        <option value="2">Мужской</option>
+        <option value="1">Женский</option>
       </Select>
 
 
@@ -221,39 +208,50 @@ let $=this.state.formReg;
         <option value="2">Мышечный/средний</option>
         <option value="3">Эндоморф/полный</option>
       </Select>
-      <Select top="Характер работы" placeholder="Выберите" name="typefig" value={$.typefig} onChange={this.ChangeSt}>
-        <option value="1">Сидячий</option>
-        <option value="2">Малоактивный</option>
-        <option value="3">Активный</option>
-        <option value="3">Тяжелый физический труд</option>
+      <Select top="Характер работы" placeholder="Выберите" name="typejob" value={$.typejob} onChange={this.ChangeSt}>
+        <option value="1">Сидячий, малоподвижный</option>
+        <option value="2">Лёгкая активность/разъездная работа/весь день на ногах </option>
+        <option value="3">Активный - лёгкий физический труд - повара/парикмахеры</option>
+        <option value="4">Тяжелый физический труд/строительные работы/грузчик</option>
       </Select>
+			<Select top="Повседневная активность" placeholder="Выберите" name="dailyActivity" value={$.dailyActivity} onChange={this.ChangeSt}>
+        <option value="1">Минимальная (сидячая работа/ноль движения)</option>
+        <option value="2">Ниже среднего (прогулки, ежедневный поход за покупками)</option>
+        <option value="3">На ногах большую часть дня</option>
+        <option value="4">Легкий ручной труд</option>
+				<option value="5">Тяжелый ручной труд</option>
+      </Select>
+      <Input top="Есть ли у Вас гормональные нарушения?" name="hormonalDisorder" value={$.hormonalDisorder} onChange={this.ChangeSt}/>
 
-    	<Select top="Уровень стресса" placeholder="Выберите" name="typefig" value={$.typefig} onChange={this.ChangeSt}>
-        <option value="1">Нет стресса</option>
-        <option value="2">Низкий</option>
-        <option value="3">Средний</option>
-        <option value="3">Тяжелый</option>
-      </Select>
+
+	<Select top="Уровень стресса" placeholder="Выберите" name="stressLevel" value={$.stressLevel} onChange={this.ChangeSt}>
+				<option value="1">Без стресса (только переживаю очень во время просмотра новостей по телевизору)</option>
+				<option value="2">По случаю (например студент во время сессии)</option>
+				<option value="3">Средний (полноценная работа с дедлайнами и контактами с другими людьми)</option>
+				<option value="3">Высокий (руководящая работа/жёсткий график/высокая ответственность)</option>
+			</Select>
 
 		<Slider step={0.5} min={4} max={14} top={"Количество часов сна в среднем " + (($.sleep)?$.sleep:"") } value={Number($.sleep)} onChange={val =>{$['sleep']=val;this.setState({formReg: $})}} />
 
 
-		 { this.state.target==1 ?
+		 { this.state.formReg.myTarget==1 ?
        <Select top="Вы растолстели быстро (3-6 мес.) или вес/жир копился годами?" placeholder="Выберите"
 				 name="fatquick" value={$.fatquick} onChange={this.ChangeSt}>
         <option value="1">Быстро</option>
         <option value="2">Медленно</option>
+				<option value="2">Нет проблемы/я на массонабор</option>
       	</Select>: null }
-      	 { this.state.target==1 ?
+      	 { this.state.formReg.myTarget==1 ?
       	<Select top="Были ли Вы в детстве полным ребенком?" placeholder="Выберите"
 					name="fatchild" value={$.fatchild} onChange={this.ChangeSt}>
         <option value="1">Нет</option>
         <option value="2">Да</option>
       	</Select> : null }
+				{ this.state.formReg.sex=='1' ?
+		          <Checkbox name="menses" value={$.menses} onChange={this.ChangeSt}>Были ли месячные на этой неделе? </Checkbox>
+		 					:''}
 
-		<Input type="email" top="E-mail" name="email" value={$.email} onChange={this.ChangeSt} />
 
-      <Checkbox>Согласен со всем <Link>этим</Link></Checkbox>
       <Button size="xl" onClick={() =>this.setState({activePanel: 'measurement'})}>Перейти к измерениям</Button>
     </FormLayout>
   </Panel>
@@ -266,18 +264,18 @@ let $=this.state.formReg;
          <div style={{'text-align': "center"}}>
            <br /><img src='https://moiglaza.com/wp-content/uploads/2017/03/lehglau-300x210.jpg' />
          </div>
-         <Input top="Обхват шеи, см"  type="number"  />
+         <Input top="Обхват шеи, см"  type="number" name="girthNeck" value={$.girthNeck} onChange={this.ChangeSt}  />
 
          <div style={{'text-align': "center"}}>
            <br /><img src='https://moiglaza.com/wp-content/uploads/2017/03/lehglau-300x210.jpg' />
          </div>
-         <Input top="Обхват талии, см"  type="number"  />
-	 { this.state.sex==1 ?
+         <Input top="Обхват талии, см"  type="number" name="girthWaist" value={$.girthWaist} onChange={this.ChangeSt} />
+	 { this.state.formReg.sex=='1' ?
          <div style={{'text-align': "center"}} >
            <br /><img src='https://moiglaza.com/wp-content/uploads/2017/03/lehglau-300x210.jpg' />
 
 
-           <Input  top="Обхват бедер, см"  type="number"  />
+           <Input  top="Обхват бедер, см"  type="number" name="girthHits" value={$.girthHits} onChange={this.ChangeSt} />
 				 	</div>
 					:''}
 				 </Div>
